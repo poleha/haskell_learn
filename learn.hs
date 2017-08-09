0ad332cc6fd77256cc9ff4232c85e2f651f0d921
Alex Polekha <a.polekha@gmail.com>
2017-08-09 17:29:34 +0300

    New examples added

--- learn.hs
+++ learn.hs
@@ -336,3 +336,105 @@ yesnoIf yesnoVal yesResult noResult = if yesno yesnoVal then yesResult else noRe
 main = print(a)
     where
         a = yesnoIf [] "YEAH!" "NO!"
+
+
+# IO
+#****************************************
+
+putStr :: String -> IO ()
+putStr [] = return ()
+putStr (x:xs) = do
+    putChar x
+    putStr xs
+
+#****************************
+print1 a = putStrLn . show $ a
+
+main = print1(a)
+    where
+        a = [1, 2, 3]
+
+
+# Solving problems
+#**********************************
+-- Reverse polish notation
+-- http://learnyouahaskell.com/functionally-solving-problems
+
+solveRPN :: (Num a, Read a) => String -> a
+solveRPN = head . foldl foldingFunction [] . words
+    where   foldingFunction (x:y:ys) "*" = (x * y):ys
+            foldingFunction (x:y:ys) "+" = (x + y):ys
+            foldingFunction (x:y:ys) "-" = (y - x):ys
+            foldingFunction xs numberString = read numberString:xs
+
+#**********************************
+
+{-
+http://learnyouahaskell.com/functionally-solving-problems
+There are two main roads going from Heathrow to London and there's a number
+of regional roads crossing them. It takes you a fixed amount of time to travel
+from one crossroads to another. It's up to you to find the optimal path to take so
+that you get to London as fast as you can! You start on the left side and can either
+cross to the other main road or go forward.
+-}
+
+import Data.List
+
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
+data Section = Section { getA :: Int, getB :: Int, getC :: Int } deriving (Show)
+type RoadSystem = [Section]
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+    let priceA = sum $ map snd pathA
+        priceB = sum $ map snd pathB
+        forwardPriceToA = priceA + a
+        crossPriceToA = priceB + b + c
+        forwardPriceToB = priceB + b
+        crossPriceToB = priceA + a + c
+        newPathToA = if forwardPriceToA <= crossPriceToA
+                        then (A,a):pathA
+                        else (C,c):(B,b):pathB
+        newPathToB = if forwardPriceToB <= crossPriceToB
+                        then (B,b):pathB
+                        else (C,c):(A,a):pathA
+    in  (newPathToA, newPathToB)
+
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+    let (bestAPath, bestBPath) = foldl roadStep ([],[]) roadSystem
+    in  if sum (map snd bestAPath) <= sum (map snd bestBPath)
+            then reverse bestAPath
+            else reverse bestBPath
+
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _ = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
+
+main = do
+    contents <- getContents
+    let threes = groupsOf 3 (map read $ lines contents)
+        roadSystem = map (\[a,b,c] -> Section a b c) threes
+        path = optimalPath roadSystem
+        pathString = concat $ map (show . fst) path
+        pathPrice = sum $ map snd path
+    putStrLn $ "The best path to take is: " ++ pathString
+    putStrLn $ "The price is: " ++ show pathPrice
+
+--cat paths.txt | runhaskell try.hs
+{-
+50
+10
+30
+5
+90
+20
+40
+2
+25
+10
+8
+0
+-}
--- try.hs
+++ try.hs
@@ -1,29 +1,68 @@
-class YesNo a where
-    yesno :: a -> Bool
+{-
+There are two main roads going from Heathrow to London and there's a number
+of regional roads crossing them. It takes you a fixed amount of time to travel
+from one crossroads to another. It's up to you to find the optimal path to take so
+that you get to London as fast as you can! You start on the left side and can either
+cross to the other main road or go forward.
+-}
 
-instance YesNo Int where
-    yesno 0 = False
-    yesno _ = True
+import Data.List
 
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
+data Section = Section { getA :: Int, getB :: Int, getC :: Int } deriving (Show)
+type RoadSystem = [Section]
 
-instance YesNo [a] where
-    yesno [] = False
-    yesno _ = True
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+    let priceA = sum $ map snd pathA
+        priceB = sum $ map snd pathB
+        forwardPriceToA = priceA + a
+        crossPriceToA = priceB + b + c
+        forwardPriceToB = priceB + b
+        crossPriceToB = priceA + a + c
+        newPathToA = if forwardPriceToA <= crossPriceToA
+                        then (A,a):pathA
+                        else (C,c):(B,b):pathB
+        newPathToB = if forwardPriceToB <= crossPriceToB
+                        then (B,b):pathB
+                        else (C,c):(A,a):pathA
+    in  (newPathToA, newPathToB)
 
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+    let (bestAPath, bestBPath) = foldl roadStep ([],[]) roadSystem
+    in  if sum (map snd bestAPath) <= sum (map snd bestBPath)
+            then reverse bestAPath
+            else reverse bestBPath
 
-instance YesNo Bool where
-    yesno = id
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _ = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
 
-instance YesNo (Maybe a) where
-    yesno (Just _) = True
-    yesno Nothing = False
+main = do
+    contents <- getContents
+    let threes = groupsOf 3 (map read $ lines contents)
+        roadSystem = map (\[a,b,c] -> Section a b c) threes
+        path = optimalPath roadSystem
+        pathString = concat $ map (show . fst) path
+        pathPrice = sum $ map snd path
+    putStrLn $ "The best path to take is: " ++ pathString
+    putStrLn $ "The price is: " ++ show pathPrice
 
-
-yesnoIf :: (YesNo y) => y -> a -> a -> a
-yesnoIf yesnoVal yesResult noResult = if yesno yesnoVal then yesResult else noResult
-
-main = print(a)
-    where
-        a = yesnoIf [] "YEAH!" "NO!"
-
-
+--cat paths.txt | runhaskell try.hs
+{-
+50
+10
+30
+5
+90
+20
+40
+2
+25
+10
+8
+0
+-}
